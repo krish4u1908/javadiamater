a97a1d234e595caefac48d52a034f49fa642bac0
krish4u1908 <125118843+krish4u1908@users.noreply.github.com>
2025-10-21 13:56:02 +0530

    Convert project to Spring Boot

---
+++ src/main/java/dk/i1/diameter/DiameterApplication.java
@@ -0,0 +1,11 @@
+package dk.i1.diameter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiameterApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DiameterApplication.class, args);
+    }
+}
